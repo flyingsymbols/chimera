4f745ca9e7f766c33dabc702f0398808f87b2942
David Hogarty <david.hogarty@gmail.com>
2009-03-20 22:52:15 -0400

    played around with sweetobjects, now focusing on decorators as a means of getting identifiers without needing to quote

--- sweetobjects.py
+++ sweetobjects.py
@@ -2,51 +2,51 @@
 # users home directory as json, also clean to/from between basic python
 # data structures (dict, list, int, float, str, etc)
 
+# implemented as a decorator over functions
+# don't need to show defaults... but defaults can change
+
+class SweetObject(object): # placeholder
+    pass
+
 from util.func_util import args_defaults_vars
 from util.format_util import script_str
 
-class SweetType(type):
-    def __init__(cls, name, bases, curr_dict):
-        type.__init__(cls, name, bases, curr_dict)
-        if curr_dict.has_key('__init__'):
-            a, d, v = args_defaults_vars(curr_dict['__init__'])
-            attrs = a[1:] # skip self
-            format_str = '%s(%s)' % (name,
-                ', '.join('%s=%%r' for a in attrs))
-            def new_repr(self):
-                format_str % [getattr(self, a) for a in attrs]
-                
-            cls.__repr__ = new_repr
+def create_type(f):
+    args, defaults, v = args_defaults_vars(f)
+    class_name = f.__name__
+    if globals().has_key(class_name):
+        raise TypeError, '%s already exists' % (class_name,)
+    positionals = args[0:-len(defaults)]
+    keywords = args[-len(defaults):]
+    total = len(args)
+    
+    class klass(SweetObject):
+        def __init__(self, *pvals, **kwvals):
+            if len(kwvals) + len(pvals) > total:
+                raise TypeError, "Invalid number of parameters"
+            else:
+                self.__dict__ = dict(zip(args, pvals))
+            for n,v in kwvals:
+                if n in args:
+                    self.__dict__[n] = v
+                else:
+                    raise TypeError, \
+                        'Invalid keyword parameter %r' % (name,)
+
+        def __repr__(self):
+            param_str = ','.join(['%s=%r' % (n, self.__dict__[n])
+                for n in args])
+            return '%s(%s)' % (self.__class__.__name__, param_str)
+
+        def __eq__(self, other):
+            return self.__dict__ == other.__dict__
+    
+    klass.__name__ = class_name
+    return klass
+    
 
 
-# introspection borrowed from ideas here:
-# http://wordaligned.org/articles/echo
-
-# want to be able to construct arbitrary Types such that
-# they can be subcontained in ordered and unordered ways,
-# they can be un-indexed or indexed by some arbitrary function
-# temporary end-goal:
-# want to be able to have a PostgresUser type that has
-# username :: str
-# password :: str
-# superuser :: boolean
-# databases :: defaultset([PostgresDatabase])
-
-a = []
 
 
-class SweetObject(object):
-    __metaclass__ = SweetType
 
-class PostgresUser(SweetObject):
-    def __init__(self, username, password, superuser=False):
-        a = 5
-        b = 2
-        
-func_code = script_str("""
-    def __init__(self, a, b, c = None):
-        self.a = a
-        self.b = b
-        self.c = c
-""")
 
