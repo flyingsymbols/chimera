3b594b6770e39632f5505f14a1d6cb06d940a1bd
David Hogarty <david.hogarty@gmail.com>
2009-03-27 22:28:17 -0400

    adding default parameters and simple test

---
+++ testsweetobjects.py
@@ -0,0 +1,19 @@
+from sweetobjects import create_type
+
+@create_type
+def Foo(a, b, c=50): pass
+
+def test_basic():
+    f = Foo(1, 2)
+    f2 = Foo(b = 2, a = 1)
+    f3 = Foo(1, 2, c = 50)
+    f4 = Foo(1, 2, c = 3)
+    assert f == f2
+    assert f2 == f3
+    assert f.a == 1
+    assert f.b == 2
+    assert f.c == 50
+    assert f != f4
+    assert f4.c == 3
+
+
